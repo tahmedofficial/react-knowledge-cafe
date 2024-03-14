be531c1cf7dd8f8b690aaa787788970fd2f083d1
tahmedofficial <tanvirofcuo@gmail.com>
2024-03-14 10:21:05 +0600

    added json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
